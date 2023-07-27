ce7b539ffa133ec4aab2fdf10e34a1b840c28ee0
extrawurst <mail@rusticorn.com>
2023-07-27 12:33:57 +0200

    fix script

--- runlocal.sh
+++ runlocal.sh
@@ -1,1 +1,1 @@
-docker run --rm --label=jekyll --volume="$PWD":/srv/jekyll:cached -it -p 4000:4000 jekyll/jekyll jekyll serve --source /srv/jekyll --watch --incremental --drafts --unpublished --future --config _config.local.yml 
+docker run --rm --label=jekyll --volume="$PWD":/site -it -p 4000:4000 bretfisher/jekyll serve -H 0.0.0.0 -P 4000 --incremental --drafts --unpublished --config _config.local.yml 
